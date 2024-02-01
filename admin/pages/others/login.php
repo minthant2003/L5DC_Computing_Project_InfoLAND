4da43e8a3cbcb87c8a108a8c30b69480e308e0a4
Min Thant <minnthantwinn@gmail.com>
2024-02-02 02:31:17 +0630

    login page modified

--- admin/pages/others/login.php
+++ admin/pages/others/login.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Purple Admin</title>
+    <title>InfoLAND | Online Learning Platform</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="../../assets/vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="../../assets/vendors/css/vendor.bundle.base.css">
@@ -16,7 +16,7 @@
     <!-- Layout styles -->
     <link rel="stylesheet" href="../../assets/css/style.css">
     <!-- End layout styles -->
-    <link rel="shortcut icon" href="../../assets/images/favicon.ico" />
+    <link rel="shortcut icon" href="../../assets/images/tabicon.ico" />
   </head>
   <body>
     <div class="container-scroller">
@@ -28,17 +28,16 @@
                 <div class="brand-logo">
                   <img src="../../assets/images/logo.png">
                 </div>
-                <h4>Hello! let's get started</h4>
-                <h6 class="font-weight-light">Sign in to continue.</h6>
-                <form class="pt-3">
+                <h4>Welcome again InfoLAND admin!</h4>
+                <form action="#" method="post" class="pt-3">
                   <div class="form-group">
-                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                    <input type="text" class="form-control form-control-lg" id="" placeholder="Enter Username">
                   </div>
                   <div class="form-group">
-                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" class="form-control form-control-lg" id="" placeholder="Enter Password">
                   </div>
                   <div class="mt-3">
-                    <a class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" href="../../index.php">SIGN IN</a>
+                    <button type="submit" class="btn btn-gradient-primary me-2">SIGN IN</button>
                   </div>
                   <div class="my-2 d-flex justify-content-between align-items-center">
                     <div class="form-check">
@@ -46,13 +45,7 @@
                         <input type="checkbox" class="form-check-input"> Keep me signed in </label>
                     </div>
                     <a href="#" class="auth-link text-black">Forgot password?</a>
-                  </div>
-                  <div class="mb-2">
-                    <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                      <i class="mdi mdi-facebook me-2"></i>Connect using facebook </button>
-                  </div>
-                  <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="register.php" class="text-primary">Create</a>
-                  </div>
+                  </div>                  
                 </form>
               </div>
             </div>
