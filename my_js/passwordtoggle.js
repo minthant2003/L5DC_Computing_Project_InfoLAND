61b998b16a643bd697bfc6c77c7973a4b58c16ec
Min Thant <minnthantwinn@gmail.com>
2024-03-03 02:11:34 +0630

    password show and hide modification

--- my_js/passwordtoggle.js
+++ my_js/passwordtoggle.js
@@ -1,17 +1,21 @@
-const passlogin = document.getElementById("passwordlogin");
-const togglelogin = document.getElementById("togglelogin");
-const passreg = document.getElementById("passwordreg");
-const togglereg = document.getElementById("togglereg");
-const passcon = document.getElementById("confirm-password");
-const togglecon = document.getElementById("toggleconfirm");
+document.addEventListener('DOMContentLoaded', () => {
+    const passlogin = document.getElementById("passwordlogin");
+    const togglelogin = document.getElementById("togglelogin");
+    const passreg = document.getElementById("passwordreg");
+    const togglereg = document.getElementById("togglereg");
+    const passcon = document.getElementById("confirm-password");
+    const togglecon = document.getElementById("toggleconfirm");
 
-const process = (input, icon) => {
-    var type = input.getAttribute('type') === 'password' ? 'text' : 'password';
-    input.setAttribute('type', type);
+    const process = (input, toggle) => {
+        var type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+        input.setAttribute('type', type);
 
-    icon.classList.toggle('fa-eye-slash');
-}
+        toggle.children[0].classList.toggle('fa-eye-slash');
+        let output = toggle.children[1].innerHTML === 'Show' ? 'Hide' : 'Show';
+        toggle.children[1].innerHTML = output;
+    }
 
-togglelogin.addEventListener('click', () => process(passlogin, togglelogin));
-togglereg.addEventListener('click', () => process(passreg, togglereg));
-togglecon.addEventListener('click', () => process(passcon, togglecon));
+    togglelogin.addEventListener('click', () => process(passlogin, togglelogin));
+    togglereg.addEventListener('click', () => process(passreg, togglereg));
+    togglecon.addEventListener('click', () => process(passcon, togglecon));
+})
