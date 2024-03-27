98c233f97cf1b37008aa508bf476f37f50433e3c
Min Thant <minnthantwinn@gmail.com>
2024-03-27 19:02:39 +0630

    add whether enrolled or not logic

--- addToShoppingCart.php
+++ addToShoppingCart.php
@@ -4,11 +4,11 @@
 
     if ($_SERVER['REQUEST_METHOD'] === "GET" && isset($_GET['id'])) {
         // Course ID to check Entry LP
-        $id = $_GET['id'];
+        $courseID = $_GET['id'];
 
         $sql = "SELECT * FROM course WHERE Course_ID=?";
         $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_bind_param($stmt, "i", $courseID);
         mysqli_stmt_execute($stmt);
     
         $result = mysqli_stmt_get_result($stmt);
@@ -18,21 +18,33 @@
         
         // Check whether Logged in or Registered
         if (isset($_SESSION['learner_authorised']) && $_SESSION['learner_authorised'] === true && isset($_SESSION['learner'])) {
-            // Check Entry LP
-            if ($_SESSION['learner']['LP'] >= $course['Entry_LP']) {
-                $response['msg'] = "Added to the Shopping Cart.";
-                $response['valid'] = true;
+            $learnerID = $_SESSION['learner']['id'];
 
+            $sql = "SELECT * FROM enroll WHERE Learner_ID=? AND Course_ID=?";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "ii", $learnerID, $courseID);
+            mysqli_stmt_execute($stmt);
+
+            $result = mysqli_stmt_get_result($stmt);
+
+            // Check whether already Enrolled or Not
+            if (mysqli_num_rows($result) === 1) {
+                $response['msg'] = "You have already Enrolled the Course.";
                 echo json_encode($response);
             } else {
-                $response['msg'] = "Require more LP. Entry LP is " . $course['Entry_LP'] . ".";
-
-                echo json_encode($response);
+                // Check Entry LP
+                if ($_SESSION['learner']['LP'] >= $course['Entry_LP']) {
+                    $response['msg'] = "Added to the Shopping Cart.";
+                    $response['valid'] = true;
+                    echo json_encode($response);
+                } else {
+                    $response['msg'] = "Require more LP. Entry LP is " . $course['Entry_LP'] . ".";
+                    echo json_encode($response);
+                }
             }
         } else {
             $response['msg'] = "Please Login or Register first.";
             $response['need_auth'] = true;
-
             echo json_encode($response);
         }
     }
@@ -41,3 +53,4 @@
 
     // case 1. Authorised or Unauthorised
     // case 2. Qualified enough for the Desired Course
+    // case 3. Already enrolled or Not?
